8a3f900557312039ad94633f72a2f898a6aa4b5a
Utkarsh Dubey <utkarshdubey44@gmail.com>
2023-08-04 17:57:24 +0530

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+  devise_for :users
 
   resources :personal_details
   root 'personal_details#index', as: 'details'
